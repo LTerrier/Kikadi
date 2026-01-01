9346368f2d00ece8a59c64afacc54e5686309395
LTerrier <77781399+LTerrier@users.noreply.github.com>
2026-01-01 14:28:41 +0100

    Update kikadi_script.js

--- kikadi_script.js
+++ kikadi_script.js
@@ -12,7 +12,38 @@
                     "Bon, vous faites deux cents pas sur le chemin et vous montez la garde au cas où quelqu'un approche.",
                     "Je pense que vous glandouillez bien assez comme ça dans la réalité pour qu’on puisse se permettre d’optimiser le fictionnel",
                     "Que vous soyez débile, c’est une chose, mais là, il y a de la mauvaise volonté",
-                    "Nouvelle technique : on passe pour des cons, les autres se marrent et on frappe. C'est nouveau."
+                    "Nouvelle technique : on passe pour des cons, les autres se marrent et on frappe. C'est nouveau.",
+                    "Cassez-vous ! Cassez-vous ! Cassez-vous ! Décarrez d’chez moi, bande de clampins !",
+                    "C'est du patrimoine ça ?",
+                    "Y a pas à dire, dès qu'il y a du dessert, le repas est tout de suite plus chaleureux !",
+                    "Il commence à doucement me faire chier celui là aussi!"
+                    "Sortez-vous les doigts du cul !!!",
+                    "Ah, mais des tanches pareilles, on devrait les mettre sous verre, hein !",
+                    "Au bout d'un moment, il est vraiment druide, c'mec-là, ou ça fait quinze ans qu'il me prend pour un con ?",
+                    "Ah ! ben ça… i'faut se méfier, avec les mecs à cran !…",
+                    "On se fait couper les deux bras, on revient vous voir et 5 minutes après on retourne se mettre sur la gueule ! Mettez-vous à la place des ennemis, c'est désespérant.",
+                    "Non, moi j'crois qu'il faut qu’vous arrêtiez d'essayer d'dire des trucs. Ça vous fatigue, déjà, et pour les autres, vous vous rendez pas compte de c'que c'est. Moi quand vous faites ça, ça me fout une angoisse... j'pourrais vous tuer, j'crois. De chagrin, hein ! J'vous jure c'est pas bien. Il faut plus que vous parliez avec des gens.",
+                    "La prochaine fois que vous faites venir un barde, je lui ouvre le bide de là à là, j'lui sors les boyaux et je file sa langue à bouffer aux chiens. C'est clair, ça ?",
+                    "Ça vous fait pas mal à la tête de glandouiller vingt-quatre heures sur vingt-quatre ?",
+                    "Merlin, i'sait déjà pas monter des blancs en neige, alors préparer une potion de polymorphie… Permettez-moi d'avoir des doutes.",
+                    "De toutes façons, c'est ma mère, j'vais pas la faire tabasser par la garde.",
+                    "J'suis chef de guerre moi, j'suis pas là pour agiter des drapeaux et jouer d'la trompette...",
+                    "Un jour je vais lui fumer sa gueule à ce connard !",
+                    "Il faut affranchir nos compagnons. Vous vous souviendrez ?",
+                    "Quel tas de merdier... Non mais sérieusement, quel merdier ! Vous vous croyez où?",
+                    "Vous, vous devriez arrêter de sourire. J'vous promets; ça devient vraiment malsain.",
+                    "Quand vous voyez des gadins de 15 pieds de haut, plantés en rond, figurez-vous qu'ils ont pas poussé dans la nuit.",
+                    "Vil félon, où as-tu dissimulé le Graal Sacré ? Dans ton cul ! Tu paieras pour cet affront !",
+                    "Une heure avant, je me dis toujours : « Tiens, je vais déjeuner avec Perceval, ça me fait plaisir ». [...] Ça vous la coupe, ça, hein ? Bon, après, une fois que j'ai bouffé avec vous, je regrette, hein, on est d'accord. Arrivé au milieu du repas, j'ai toujours envie de vous éclater le crâne avec le tranchant de la coupe, là, vous voyez, pour vous faire... fermer votre gueule une bonne fois pour toutes... mais sinon... je vous aime.",
+                    "Mais vous êtes pas mort, espèce de connard ?",
+                    "Oubliez ce que je viens de dire. Le Graal, c'est de la merde.",
+                    "Et si je vous dis que vous êtes deux glands, là, vous avez du péremptoire. C’est vous qui voyez.",
+                    "Mais qu'est-ce que que vous faites là, trou du cul ?",
+                    "C’était quand la dernière fois qu’on s’est retrouvés tous d’accord sur un truc !?",
+                    "J'bouff'rais un ch'val en salade !!!",
+                    "MAIS C'EST PAS POSSIBLE QUE LA DAME DU LAC ELLE SACHE PAS NAGER !!!",
+                    "ARRÊTEZ DE M'APPELER SIRE !!!!!",
+                    "Qu'est-ce que c'est que quelqu'un qui souffre et qui fait couler son sang par terre pour que tout le monde soit coupable ? Tous les suicidés sont le Christ. Toutes les baignoires sont le Graal..."
                 ]
             },
             {
@@ -118,6 +149,12 @@
                     "Vous n'avez qu'à considérer que je suis officiellement cul nu !",
                     "Mais c'est un petit mâle, il s'appelle ... « Ferme ta gueule », c'est que quand il est né il arrêtait pas de couiner...",
                 ]
+            },
+            {
+               personnage: "Guethenoc",
+               citation: [
+                    "Pour le transport, vaut mieux la brouette !",
+               ]
             }
  ]
             
@@ -164,4 +201,5 @@ function verifier() {
     }, 2000);
 
 }
+
 
